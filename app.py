32e5053c02c03efe63d9542bc62857b38d343ba1
Dan Hricko <dhricko9@gmail.com>
2025-12-22 23:21:15 -0500

    creating separate service for app updates

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def hello():
     return 'Hello from Jenkins CI/CD!, This is Dan Hricko! Can you hear me now. updated changes'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
